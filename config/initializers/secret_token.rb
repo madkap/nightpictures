bf8ac6fce1d9b9e5abad3b500799c6528f4f97b5
Michael Dauz <michaeldauz@gmail.com>
2014-06-07 17:26:37 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nightpictures::Application.config.secret_key_base = 'bdf1b0644d811b4da4129f86474b1406ffe845641f1d3d28d8fda57cbc841415791ca7d118cb9fd4fac75d91095a4adcc000ba6107648d7791a66e9555112fb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nightpictures::Application.config.session_store :cookie_store, key: '_nightpictures_session'
